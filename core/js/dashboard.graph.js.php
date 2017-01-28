70bb26f42f09648583d8458d742a47297656c15a
Michael Krug <krumi@hrz.tu-chemnitz.de>
2017-01-28 18:58:26 +0100

    disable caching of dashboard data

--- core/js/dashboard.graph.js.php
+++ core/js/dashboard.graph.js.php
@@ -1,6 +1,10 @@
 <?php
 
-# Send Javascript header 
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
+# Send Javascript header
 header('Content-type: text/javascript');
 
 # Load Config 
