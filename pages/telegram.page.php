b15a358dfae5f5de03a49d928d2e93cecb722eb3
Michael Krug <krumi@hrz.tu-chemnitz.de>
2017-02-07 16:38:46 +0100

    add telegram info page

---
+++ pages/telegram.page.php
@@ -0,0 +1,117 @@
+<header id="single-header">
+        <div class="row">
+                <div class="col-md-12 text-center">
+                        <h1>
+                                <strong>Telegram</strong> Benachrichtigungen<br><small>Seltene Pokémon, wechselnde Arenen sowie aktive Lockmodule direkt auf's Handy</small>
+                        </h1>
+
+                </div>
+        </div>
+</header>
+
+<div class="row">
+
+            <div class="col-md-4 col-sm-4 col-xs-12 big-data">
+                <a href="https://t.me/pogochemnitzalerts" target="_blank">
+                    <img src="/core/img/telegramicon.png" alt="Telegram Pokémon Alarm Channel" width="50" class="big-icon">
+                    <p><big><strong>@PoGoChemnitzAlerts</strong></big><br>Pokémon</p>
+                </a>
+            </div>
+
+            <div class="col-md-4 col-sm-4 col-xs-12 big-data" style="border-right:1px lightgray solid;border-left:1px lightgray solid;">
+                <a href="https://t.me/pogochemnitzgyms" target="_blank">
+                    <img src="/core/img/telegramicon.png" alt="Telegram Arenen Alarm Channel" width="50" class="big-icon">
+                    <p><big><strong>@PoGoChemnitzGyms</strong></big><br>Arenen</p>
+                </a>
+            </div>
+
+            <div class="col-md-4 col-sm-4 col-xs-12 big-data">
+                <a href="https://t.me/pogochemnitzlures" target="_blank">
+                    <img src="/core/img/telegramicon.png" alt="Telegram Lockmodule Alarm Channel" width="50" class="big-icon">
+                    <p><big><strong>@PoGoChemnitzLures</strong></big><br>Lockmodule</p>
+                </a>
+            </div>
+
+</div>
+
+<div class="row" style="text-align:center">
+
+        <h2>Telegram? Bitte was?</h2>
+
+        <p>Telegram ist ein wunderbarer, multifunktionaler Messenger - für das Smartphone als auch am Desktop. Hier geht's zum Download: <a href="https://telegram.org/">Telegram</a></p>
+        <br>
+
+        <h2>Über was kann ich mich benachrichtigen lassen?</h2>
+
+        <h3><a href="https://t.me/pogochemnitzalerts">@PoGoChemnitzAlerts</a></h3>
+        <p>Dieser Channel benachrichtigt dich über seltene Pokémon.<br>Dir werden sowohl IV-Wert, das Moveset, sowie natürlich genauer Spawnpoint und Verweildauer angezeigt.</p>
+        <h3>Aktuell sind folgende Pokémon auf unserer Watch-List:</h3>
+        <div>
+            <img src="core/pokemons/3.png" width=60>
+            <img src="core/pokemons/6.png" width=60>
+            <img src="core/pokemons/9.png" width=60>
+            <img src="core/pokemons/24.png" width=60>
+            <img src="core/pokemons/26.png" width=60>
+            <img src="core/pokemons/28.png" width=60>
+            <img src="core/pokemons/31.png" width=60>
+            <img src="core/pokemons/34.png" width=60>
+            <img src="core/pokemons/38.png" width=60>
+            <img src="core/pokemons/45.png" width=60>
+            <img src="core/pokemons/51.png" width=60>
+            <img src="core/pokemons/57.png" width=60>
+            <img src="core/pokemons/59.png" width=60>
+            <img src="core/pokemons/62.png" width=60>
+            <img src="core/pokemons/64.png" width=60>
+            <img src="core/pokemons/65.png" width=60>
+            <img src="core/pokemons/67.png" width=60>
+            <img src="core/pokemons/68.png" width=60>
+            <img src="core/pokemons/71.png" width=60>
+            <img src="core/pokemons/76.png" width=60>
+            <img src="core/pokemons/78.png" width=60>
+            <img src="core/pokemons/80.png" width=60>
+            <img src="core/pokemons/83.png" width=60>
+            <img src="core/pokemons/85.png" width=60>
+            <img src="core/pokemons/88.png" width=60>
+            <img src="core/pokemons/89.png" width=60>
+            <img src="core/pokemons/91.png" width=60>
+            <img src="core/pokemons/94.png" width=60>
+            <img src="core/pokemons/95.png" width=60>
+            <img src="core/pokemons/103.png" width=60>
+            <img src="core/pokemons/106.png" width=60>
+            <img src="core/pokemons/107.png" width=60>
+            <img src="core/pokemons/110.png" width=60>
+            <img src="core/pokemons/112.png" width=60>
+            <img src="core/pokemons/113.png" width=60>
+            <img src="core/pokemons/114.png" width=60>
+            <img src="core/pokemons/115.png" width=60>
+            <img src="core/pokemons/128.png" width=60>
+            <img src="core/pokemons/130.png" width=60>
+            <img src="core/pokemons/131.png" width=60>
+            <img src="core/pokemons/134.png" width=60>
+            <img src="core/pokemons/135.png" width=60>
+            <img src="core/pokemons/136.png" width=60>
+            <img src="core/pokemons/139.png" width=60>
+            <img src="core/pokemons/141.png" width=60>
+            <img src="core/pokemons/142.png" width=60>
+            <img src="core/pokemons/143.png" width=60>
+            <img src="core/pokemons/144.png" width=60>
+            <img src="core/pokemons/145.png" width=60>
+            <img src="core/pokemons/146.png" width=60>
+            <img src="core/pokemons/147.png" width=60>
+            <img src="core/pokemons/148.png" width=60>
+            <img src="core/pokemons/149.png" width=60>
+            <img src="core/pokemons/150.png" width=60>
+            <img src="core/pokemons/151.png" width=60>
+        </div>
+
+        <br>
+
+        <h3><a href="https://t.me/pogochemnitzgyms">@PoGoChemnitzGyms</a></h3>
+        <p>Dieser Channel benachrichtigt dich wenn Arenen besiegt wurden und von einem anderen Team nun kontrolliert werden.<br>Es werden dir das besiegt als auch das neue Team angezeigt, sowie natürlich die Position der Arena.</p>
+
+        <br>
+
+        <h3><a href="https://t.me/pogochemnitzlures">@PoGoChemnitzLures</a></h3>
+        <p>Dieser Channel benachrichtigt dich wenn irgendwo in Chemnitz ein Lockmodul gezündet wurde.<br>Es werden dir Restzeit sowie die Position auf einer Karte dargestellt.</p>
+
+</div>
