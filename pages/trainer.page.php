f3ce9027c334b98eb2f03cb77f6b042a6833c341
Michael Krug <krumi@hrz.tu-chemnitz.de>
2017-02-17 22:15:02 +0100

    remove mobile trainers form

--- pages/trainer.page.php
+++ pages/trainer.page.php
@@ -11,7 +11,7 @@
 	<div class="col-md-12 text-center">
 		<form class="form-inline" id="searchTrainer" method="GET">
 		  <div class="form-group">
-			<div class="input-group hidden-xs">
+			<div class="input-group">
 				<div class="input-group-btn">
 					<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" id="teamSelector"><span id="teamSelectorText"><img src="core/img/map_white.png" />&nbsp;<?= $locales->TRAINERS_SEARCH_ALL_TEAMS ?></span>&nbsp;<span class="caret"></span></button>
 					<ul class="dropdown-menu">
@@ -23,26 +23,6 @@
 				</div>
 				<input type="text" class="form-control" name="name" id="name" placeholder="Name" value="">
 				<div class="input-group-btn">
-					<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"  id="rankingSelector"><span id="rankingOrderText"><?= $locales->TRAINERS_SEARCH_LEVELS_FIRST ?></span>&nbsp;<span class="caret"></span></button>
-					<ul class="dropdown-menu dropdown-menu-right">
-					  <li><a class="rankingOrderItems" id="levelsFirst" href="#">&nbsp;<?= $locales->TRAINERS_SEARCH_LEVELS_FIRST ?></a></li>
-					  <li><a class="rankingOrderItems" id="gymsFirst" href="#">&nbsp;<?= $locales->TRAINERS_SEARCH_GYMS_FIRST ?></a></li>
-					  <li><a class="rankingOrderItems" id="maxCpFirst" href="#">&nbsp;<?= $locales->TRAINERS_SEARCH_MAX_CP_FIRST ?></a></li>
-					</ul>
-				</div>
-			</div>
-			  <div class="input-group-vertical visible-xs">
-				<div class=" ">
-					<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" id="teamSelector"><span id="teamSelectorText"><img src="core/img/map_white.png" />&nbsp;<?= $locales->TRAINERS_SEARCH_ALL_TEAMS ?></span>&nbsp;<span class="caret"></span></button>
-					<ul class="dropdown-menu">
-						<li><a class="teamSelectorItems" id="AllTeamsFilter" href="#"><img src="core/img/map_white.png" />&nbsp;<?= $locales->TRAINERS_SEARCH_ALL_TEAMS ?></a></li>
-						<li><a class="teamSelectorItems" id="BlueTeamFilter" href="#"><img src="core/img/map_blue.png" />&nbsp;<?= $locales->MYSTIC ?></a></li>
-						<li><a class="teamSelectorItems" id="RedTeamFilter" href="#"><img src="core/img/map_red.png" />&nbsp;<?= $locales->VALOR ?></a></li>
-						<li><a class="teamSelectorItems" id="YellowFilter" href="#"><img src="core/img/map_yellow.png" />&nbsp;<?= $locales->INSTINCT ?></a></li>
-					</ul>
-				</div>
-				<input type="text" class="form-control" name="name" id="name" placeholder="Name" value="">
-				<div class="">
 					<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"  id="rankingSelector"><span id="rankingOrderText"><?= $locales->TRAINERS_SEARCH_LEVELS_FIRST ?></span>&nbsp;<span class="caret"></span></button>
 					<ul class="dropdown-menu dropdown-menu-right">
 					  <li><a class="rankingOrderItems" id="levelsFirst" href="#">&nbsp;<?= $locales->TRAINERS_SEARCH_LEVELS_FIRST ?></a></li>
