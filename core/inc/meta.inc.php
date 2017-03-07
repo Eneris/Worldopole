beed824752131e30eb6f8bd8938130c37faf396a
Michael Krug <krumi@hrz.tu-chemnitz.de>
2017-03-07 12:46:18 +0100

    add terms page

--- core/inc/meta.inc.php
+++ core/inc/meta.inc.php
@@ -235,6 +235,30 @@
 		
 		<?php
 			break;
+		
+		case 'terms':
+		?>
+		
+		<title><?= $config->infos->site_name ?> | <?= $locales->TERMS_METATITLE ?></title>
+		
+		<link rel="canonical" href="<?= HOST_URL ?>/terms" />
+		<base href="<?= HOST_URL ?>">
+
+		<meta property="og:locale" content="en_US" />
+		<meta property="og:type" content="website" />
+		<meta property="og:title" content="<?= $config->infos->site_name ?> | <?= $locales->TERMS_METATITLE ?>" />
+		<meta property="og:description" content="<?= $locales->TERMS_METADESC ?>" />
+		<meta property="og:url" content="<?= HOST_URL ?>/terms" />
+		<meta property="og:site_name" content="<?= $config->infos->site_name ?>" />
+		<meta property="og:image" content="<?= HOST_URL ?>core/img/logo.jpg" />
+		
+		<meta name="twitter:card" content="summary" />
+		<meta name="twitter:description" content="<?= $locales->TERMS_METADESC ?>" />
+		<meta name="twitter:title" content="<?= $config->infos->site_name ?> | <?= $locales->TERMS_METATITLE ?>" />
+		<meta name="twitter:image" content="<?= HOST_URL ?>core/img/logo.jpg" />
+		
+		<?php
+			break;
 	}
 }
 	
