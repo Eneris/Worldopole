5f4a4bd32ccded94a847c613abf729741eb34b21
Michael Krug <krumi@hrz.tu-chemnitz.de>
2017-04-17 21:23:26 +0200

    fix title for nests

--- core/inc/meta.inc.php
+++ core/inc/meta.inc.php
@@ -263,14 +263,14 @@
 		case 'nests':
 		?>
 		
-		<title><?= $config->infos->site_name ?> | <?= $locales->NAV_NESTS ?></title>
+		<title><?= $config->infos->site_name ?> | <?= $locales->NESTS_METADESC ?></title>
 		
 		<link rel="canonical" href="<?= HOST_URL ?>nests" />
 		<base href="<?= HOST_URL ?>">
 
 		<meta property="og:locale" content="en_US" />
 		<meta property="og:type" content="website" />
-		<meta property="og:title" content="<?= $config->infos->site_name ?> | <?= $locales->NAV_NESTS ?>" />
+		<meta property="og:title" content="<?= $config->infos->site_name ?> | <?= $locales->NESTS_METADESC ?>" />
 		<meta property="og:description" content="<?= $locales->NESTS_METADESC ?>" />
 		<meta property="og:url" content="<?= HOST_URL ?>nests" />
 		<meta property="og:site_name" content="<?= $config->infos->site_name ?>" />
@@ -278,7 +278,7 @@
 		
 		<meta name="twitter:card" content="summary" />
 		<meta name="twitter:description" content="<?= $locales->NESTS_METADESC ?>" />
-		<meta name="twitter:title" content="<?= $config->infos->site_name ?> | <?= $locales->NAV_NESTS ?>" />
+		<meta name="twitter:title" content="<?= $config->infos->site_name ?> | <?= $locales->NESTS_METADESC ?>" />
 		<meta name="twitter:image" content="<?= HOST_URL ?>core/img/logo.jpg" />
 		
 		<?php
